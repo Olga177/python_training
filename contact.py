6669a5f39daa82a87f2b0064c934878bc2bf0b9b
Olga_177 <1774659@gmail.com>
2018-06-21 07:47:50 -0700

    contact file reformatted

---
+++ contact.py
@@ -0,0 +1,25 @@
+class Contact:
+    def __init__(self, first_name, middle_name, last_name, nickname, title, company, address, home_phone, mobile_phone,
+                 work_phone, fax, email1,
+                 email2, email3, homepage, secondary_address, second_home_phone, notes):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.secondary_address = secondary_address
+        self.second_home_phone = second_home_phone
+        self.notes = notes
+
+
+
