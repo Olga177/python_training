a8e02ecd558d06e51db77f511176e32202740540
Olga_177 <1774659@gmail.com>
2018-06-24 22:44:14 -0700

    Repeated first scenario recording

--- test_add_group.py
+++ test_add_group.py
@@ -2,6 +2,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
 from group import Group
+from application import Application
 
 def is_alert_present(wd):
     try:
@@ -12,70 +13,23 @@ def is_alert_present(wd):
 
 class test_add_group(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver(capabilities={"marionette": False},
-                            firefox_binary="C:\Program Files\Mozilla Firefox ESR/firefox.exe")
-        self.wd.implicitly_wait(60)
+        self.app = Application()
+
 
     def test_add_group(self):
         success = True
-        self.login(username = "admin", password = "secret")
-        self.create_group(Group (name = "group_name", header = "group_header", footer = "group_footer"))
-        self.logout()
+        self.app.login(username = "admin", password = "secret")
+        self.app.create_group(Group (name = "group_name", header = "group_header", footer = "group_footer"))
+        self.app.logout()
 
     def test_add_empty_group(self):
         success = True
-        self.login(username = "admin", password = "secret")
-        self.create_group(Group(name = "", header = "", footer = ""))
-        self.logout()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_group_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_group_page()
-        #  Initiating first group creation
-        wd.find_element_by_name("new").click()
-        # Populating group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # Submiting group form
-        wd.find_element_by_name("submit").click()
-        self.return_to_group_page()
-
-    def open_group_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        self.app.login(username = "admin", password = "secret")
+        self.app.create_group(Group(name = "", header = "", footer = ""))
+        self.app.logout()
 
     def tearDown(self):
-        self.wd.quit()
+        self.app.destroy()
 
 if __name__ == '__main__':
     unittest.main()
