e8180507fdff433f6cb00589a6e8c36eb792e1c2
Olga_177 <1774659@gmail.com>
2018-06-24 23:51:31 -0700

    Repeated first scenario recording

--- application.py
+++ application.py
@@ -7,14 +7,24 @@ class Application:
                             firefox_binary="C:\Program Files\Mozilla Firefox ESR/firefox.exe")
         self.wd.implicitly_wait(60)
 
-
-    def logout(self):
+    def open_home_page(self):
         wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
+        wd.get("http://localhost/addressbook/")
 
-    def return_to_group_page(self):
+    def login(self, username, password):
         wd = self.wd
-        wd.find_element_by_link_text("group page").click()
+        self.open_home_page()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_group_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("groups").click()
 
     def create_group(self, group):
         wd = self.wd
@@ -35,24 +45,13 @@ class Application:
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
-    def open_group_page(self):
+    def return_to_group_page(self):
         wd = self.wd
-        wd.find_element_by_link_text("groups").click()
+        wd.find_element_by_link_text("group page").click()
 
-    def login(self, username, password):
+    def logout(self):
         wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.find_element_by_link_text("Logout").click()
 
     def destroy(self):
         self.wd.quit()
--- test_add_group.py
+++ test_add_group.py
@@ -1,35 +1,24 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
 from group import Group
 from application import Application
+import pytest
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-class test_add_group(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+def test_add_group(app):
+    success = True
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="group_name", header="group_header", footer="group_footer"))
+    app.logout()
 
 
-    def test_add_group(self):
-        success = True
-        self.app.login(username = "admin", password = "secret")
-        self.app.create_group(Group (name = "group_name", header = "group_header", footer = "group_footer"))
-        self.app.logout()
+def test_add_empty_group(app):
+    success = True
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
 
-    def test_add_empty_group(self):
-        success = True
-        self.app.login(username = "admin", password = "secret")
-        self.app.create_group(Group(name = "", header = "", footer = ""))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
-
-if __name__ == '__main__':
-    unittest.main()
